50609e5cb8098bc9cfae09a1947ae3e059f7f91e
Michael Bleed <michaelbleed@michael-bleeds-macbook-pro.local>
2010-09-03 09:47:13 -0400

    add ajax call on new encounter form to determine clinic options after a care setting is chosen

--- app/controllers/encounters_controller.rb
+++ app/controllers/encounters_controller.rb
@@ -14,8 +14,7 @@ class EncountersController < ApplicationController
   # GET /encounters/1.xml
   def show
     @encounter = Encounter.find(params[:id])
-	@encounterDiagnoses = @encounter.diagnoses
-	p @encounterDiagnoses
+	  @encounterDiagnoses = @encounter.diagnoses
 
     respond_to do |format|
       format.html # show.html.erb
@@ -26,15 +25,26 @@ class EncountersController < ApplicationController
   # GET /encounters/new
   # GET /encounters/new.xml
   def new
-	clerkship = Clerkship.find_all_by_name('Pediatrics')
+	  clerkship = Clerkship.find_all_by_name('Pediatrics')
     @encounter = Encounter.new
-	@cs = CareSetting.all
-	@clinics = Clinic.all
-	dxc = DiagnosisCategory.find_all_by_clerkship_id(clerkship.id)
+	  @cs = CareSetting.all
+	  @clinics = Clinic.all
+	  dxc = DiagnosisCategory.find_all_by_clerkship_id(clerkship.id)
 
     respond_to do |format|
       format.html # new.html.erb
       format.xml  { render :xml => @encounter }
+    end
+  end
+  
+  # GET /encounters/get_clerkships_by_care_setting/OP
+  # GET /encounters/get_clerkships_by_care_setting/OP.xml
+  def get_clerkships_by_care_setting
+	  @clinics = Clinic.find_all_by_care_setting(params[:id])
+    
+    respond_to do  |format|
+      format.html { redirect_to encounters_path }
+      format.js {render :layout => false}
     end
   end
   
@@ -79,8 +89,33 @@ class EncountersController < ApplicationController
   # POST /encounters
   # POST /encounters.xml
   def create
-    @encounter = Encounter.new(params[:encounter])
-    @encounter.period = Period.new(:name  => params[:encounter]['period'])
+	#p params[:encounter]
+	case
+		when (params[:hx]['O'] == "1" && params[:hx]['P'] == "1")
+			hx = 'B'
+		when (params[:hx]['O'] == "1" && params[:hx]['P'] == "0") 
+			hx = 'O'
+		when (params[:hx]['O'] == "0" && params[:hx]['P'] == "1")
+			hx = 'P'
+		when (params[:hx]['O'] == "0" && params[:hx]['P'] == "0")
+			hx = 'N'
+		else
+			hx = 'N'
+	end
+	p params[:hx]
+	case
+		when (params[:px]['O'] == "1" && params[:px]['P'] == "1")
+			px = 'B'
+		when (params[:px]['O'] == "1" && params[:px]['P'] == "0") 
+			px = 'O'
+		when (params[:px]['O'] == "0" && params[:px]['P'] == "1")
+			px = 'P'
+		when (params[:px]['O'] == "0" && params[:px]['P'] == "0")
+			px = 'N'
+		else 
+			px = 'N'
+	end
+    @encounter = Encounter.new("clerkship_id" => params[:encounter]['clerkship_id'], "clinic_id" => params[:encounter]['clinic_id'], "encounter_date" => params[:encounter]['encounter_date'], "patient_id" => 1, "age" => params[:encounter]['age'], "gender" => params[:encounter]['gender'], "hx" => hx, "px" => px, "notes" => params[:encounter]['notes'], "created_by" => 1, "updated_by" => 1)
 
     respond_to do |format|
       if @encounter.save
