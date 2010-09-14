8eef18c6de2aedfecf889008534d054280171f97
Amitava Shee <amitava@umich.edu>
2010-09-14 18:10:51 -0400

    added support for feedback. wired-in resource listing on right pane - must filter out irrelevant resources

--- app/controllers/encounters_controller.rb
+++ app/controllers/encounters_controller.rb
@@ -16,7 +16,7 @@ class EncountersController < ApplicationController
     @encounter = Encounter.find(params[:id])
 	  @encounterDiagnoses = @encounter.diagnoses
 	  @encounterProcedures = @encounter.procedures
-
+    @resources = Resource.find :all
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @encounter }
