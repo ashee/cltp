659a126e1323a0c6544e2f244ae44c46ed04cf03
Amitava Shee <amitava@umich.edu>
2010-09-20 16:15:53 -0400

    Refactored Encounter model so that it encapsulates logic to determine primary and secondary diagnoses

--- app/models/encounter.rb
+++ app/models/encounter.rb
@@ -9,4 +9,12 @@ class Encounter < ActiveRecord::Base
   # e = Encounter.find 1
   # e.diagnoses (looks up encounter_dx table for associated rows)
   
+  def primary_dx
+    diagnoses.find_by_dx_type('P')
+  end
+  
+  def secondary_dx
+    diagnoses.find_all_by_dx_type('S')
+  end
+  
 end
