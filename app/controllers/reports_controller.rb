a07b1927d92f81537a53c918e09f1c25bfbf5aff
lauriek334 <laurie@umich.edu>
2010-09-30 16:19:22 -0400

    Completed work on the individual diagnoses per student report with the popup menu to select which student to display the data for cleaned up the other reports.

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,46 +1,37 @@
 class ReportsController < ApplicationController
   def index
   end
-    
+  
   def encounters_by_care_settings
     @reportlinearray = Reports.encounters_by_care_settings
-    ActiveRecord::Base.logger.debug "@reportlinearray: #{@reportlinearray}"
-    
+    ActiveRecord::Base.logger.debug "@reportlinearray: #{@reportlinearray}"  
   end
 
   def dx_by_students
      @reportarray = Reports.dx_by_students
   end
-  
-  def dx_by_students
-      @reportarray = Reports.dx_by_students
-   end
  
   def hnp_observed_vs_performed
-     
-  end
-  
-  def dx_observed_vs_performed
-    @reportlinearray = Reports.dx_observed_vs_performed
-  end
-  
-  def summary_dx_observed_vs_performed
+    @reportlinearray = Reports.hnp_observed_vs_performed
   end
 
-  def student_names
-     # Need to populate the popup menu with all the users.
-     # @userlist = User.all(:select => 'lastname, firstname', 'id')
-   end
-  
-  def student_individual_dx_select
-      # Need to populate the popup menu with all the users.
-      @students = User.students
-   end
+  def dx_observed_vs_performed
+  end
+
+  def summary_dx_observed_vs_performed
+  end
    
-   def student_individual_dx
-     student_id = params[:student_id]
-     @items = Reports.student_individual_dx
-     render :layout => false
-   end
-  
+  def student_individual_dx
+    @students = User.students
+  end
+
+  def render_student_individual_dx
+    student_id = params[:student_id]
+    @student = User.find_by_id student_id
+    @items = Reports.student_individual_dx student_id
+    ActiveRecord::Base.logger.debug "@student: #{@student}"
+    ActiveRecord::Base.logger.debug "@student.fullname: #{@student.fullname}"
+    render :layout => false
+  end
+
 end
--- app/models/reports.rb
+++ app/models/reports.rb
@@ -1,10 +1,9 @@
 =begin
-  1. Numbers of Patients each student has seen on Inpatient, Outpatient and Newborn Nursery, displaying the class average etc as it does now is kind of nice too..especially for the students to be able to see how they compare.
-      *LWK* Should students be able to see this
-      *LWK* does Jenny want a faculty view which lists all students?
+  1. Numbers of Patients each student has seen on Inpatient, Outpatient and Newborn Nursery, 
+      displaying the class average etc as it does now is kind of nice too..
+      especially for the students to be able to see how they compare.      
       
-      
-  2.Individual diagnoses that each student has performed
+  2. Individual diagnoses that each student has performed
   
   3. H&P observed vs. performed on their own
   
@@ -22,8 +21,13 @@
   6. Each of the above by site
   
 =end
+
 class Reports
   
+  #--------------------------------------------
+  # Encounters by Care Setting 
+  # Numbers of Patients each student has seen on Inpatient, Outpatient and Newborn Nursery
+  #--------------------------------------------
   def self.encounters_by_care_settings
     sql = <<-EOF
       select u.username as 'Username',
@@ -41,97 +45,66 @@ class Reports
     ActiveRecord::Base.connection.select_all sql
   end
   
- 
   #-------------------------------------------
-  # self.dx_categories
-  #-------------------------------------------
-  # select u.username as 'Username',
-  # sum(if(edx.dx_id=1,1,0)) as 'Behavior',
-  # sum(if(edx.dx_id=2,1,0)) as 'Cardiology',
-  # sum(if(edx.dx_id=3,1,0)) as 'Chronic Medical Problem',
-  # sum(if(edx.dx_id=4,1,0)) as 'Dermatology',
-  # sum(if(edx.dx_id=5,1,0)) as 'Emergent Clinical Problem',
-  # sum(if(edx.dx_id=6,1,0)) as 'Gastroenterology',
-  # sum(if(edx.dx_id=7,1,0)) as 'Growth',
-  # sum(if(edx.dx_id=8,1,0)) as 'H/O',
-  # sum(if(edx.dx_id=9,1,0)) as 'Health Maintenance',
-  # sum(if(edx.dx_id=10,1,0)) as 'Infectious Disease',
-  # sum(if(edx.dx_id=11,1,0)) as 'Neurology',
-  # sum(if(edx.dx_id=12,1,0)) as 'Nutrition',
-  # sum(if(edx.dx_id=13,1,0)) as 'Respiratory',
-  # sum(if(edx.dx_id=14,1,0)) as 'Unique Condition',
-  # sum(if(edx.dx_id=15,1,0)) as 'Other',
-  #       u.firstname as 'Firstname', 
-  #       u.lastname as 'Lastname'
-  #   from encounters e 
-  #   join users u on e.created_by = u.id
-  #   join encounter_dx edx on e.created_by = u.id
-  #    where e.clerkship_id = 1
-  #   group by e.created_by;
-    
-  def self.dx_by_students
+   # Individual diagnoses that each student has performed
+   #-------------------------------------------    
+    def self.dx_by_students
     dxcats = DiagnosisCategory.all
     ActiveRecord::Base.logger.debug "dxcats: #{dxcats}"
-    
-     template = %Q{sum(if(edx.dx_id=%s,1,0)) as '%s'}
-       
-     partialSqlStatement = dxcats.map { |dxc| 
-       sprintf template, dxc["id"], dxc["name"]
-     }.join(",\n")
-    
-    
-      sql = <<-EOF
-        select 
-            u.firstname as 'Firstname', 
-            u.lastname as 'Lastname',
-            #{partialSqlStatement}
-          from encounters e 
-          join users u on e.created_by = u.id
-          join encounter_dx edx on e.created_by = u.id
-          where e.clerkship_id = 1
-          group by e.created_by;
-  
-  
-  
-      EOF
 
-      # Calling ActiveRecord::Base.connection.execute(sql) in order to get the fields of each record returned
-      # in the correct order.  Using the select_all method returns a hash of the rows but they are not ordered 
-      # correctly
-      
-      sqlResult = ActiveRecord::Base.connection.execute sql     
- 
-      # this returns a Mysql::Result object.
-      # first check whether something was returned
-      if sqlResult != nil
-        # first extract the field names
-        fieldObjectArray = sqlResult.fields
-        
-        fieldNames = Array.new
-        fieldObjectArray.each do |aField|
-            fieldNames << aField.name
-          end               
-      
-        # now extract the rows of data
-       rows = Array.new
-       sqlResult.each do |row|
-          rows << row
+    template = %Q{sum(if(edx.dx_id=%s,1,0)) as '%s'}
+
+    partialSqlStatement = dxcats.map { |dxc| 
+    sprintf template, dxc["id"], dxc["name"]
+    }.join(",\n")
+    
+    
+    sql = <<-EOF
+      select 
+          u.firstname as 'Firstname', 
+          u.lastname as 'Lastname',
+          #{partialSqlStatement}
+        from encounters e 
+        join users u on e.created_by = u.id
+        join encounter_dx edx on e.created_by = u.id
+        where e.clerkship_id = 1
+        group by e.created_by;
+    EOF
+
+    # Calling ActiveRecord::Base.connection.execute(sql) in order to get the fields of each record returned
+    # in the correct order.  Using the select_all method returns a hash of the rows but they are not ordered 
+    # correctly
+  
+    sqlResult = ActiveRecord::Base.connection.execute sql     
+
+    # this returns a Mysql::Result object.
+    # first check whether something was returned
+    if sqlResult != nil
+      # first extract the field names
+      fieldObjectArray = sqlResult.fields
+    
+      fieldNames = Array.new
+      fieldObjectArray.each do |aField|
+          fieldNames << aField.name
         end               
-        sqlResult.free;
-      end
-      
-      fieldNamesAndDataArray = Array.new
-      fieldNamesAndDataArray << fieldNames << rows
-      # FOR DEBUGGING ActiveRecord::Base.logger.debug "fieldNamesAndDataArray: #{fieldNamesAndDataArray}"
-      return fieldNamesAndDataArray
+  
+      # now extract the rows of data
+     rows = Array.new
+     sqlResult.each do |row|
+        rows << row
+      end               
+      sqlResult.free;
     end
   
-  
-  def self.hnp_observed_vs_performed
-    
+    fieldNamesAndDataArray = Array.new
+    fieldNamesAndDataArray << fieldNames << rows
+    return fieldNamesAndDataArray
   end
   
-  def self.dx_observed_vs_performed
+  #-------------------------------------------
+  # Individual diagnoses that each student has performed
+  #-------------------------------------------    
+   def self.hnp_observed_vs_performed
     sql = <<-EOF
     select 
        u.firstname as 'FirstName',
@@ -145,45 +118,42 @@ class Reports
        where e.clerkship_id = 1
        group by e.created_by;
      EOF
-    
-   # ActiveRecord::Base.logger.debug "sql: #{sql}"
+
     ret = ActiveRecord::Base.connection.select_all sql  
-   # ActiveRecord::Base.logger.debug "ret: #{ret}"
     return ret
   end
   
-  def self.summary_dx_observed_vs_performed
-  end
-  
-    def self.student_individual_dx
-      # Need to populate the popup menu with all the users.
-   
-      sql = <<-EOF
-      select distinct edx.dx_id,
-             dx.name as 'dxname'
-           from encounter_dx edx
-           join dx on edx.dx_id = dx.id
-           EOF
-      
-      user_id = 1
-      sql = <<-EOF
-      select distinct edx.dx_id,
-             dx.name as 'dxname'
-           from encounter_dx edx
-           join dx on edx.dx_id = dx.id
-           EOF
-      sql = sql + "where edx.created_by = "
-      sql = sql + user_id.to_s
-      sql = sql + " order by dx_id;"
+  #-------------------------------------------
+  # Individual diagnoses that each student has performed
+  #-------------------------------------------    
+  def self.student_individual_dx(student_id)
+    # Need to populate the popup menu with all the users.
+       
+    sql = <<-EOF
+      select distinct edx.dx_id, diagnoses.name as 'dxname'
+      from encounter_dx edx
+      join dx diagnoses on edx.dx_id = diagnoses.id
+      where edx.created_by = #{student_id}
+      order by diagnoses.id
+    EOF
 
-      ActiveRecord::Base.logger.debug "sql: #{sql}"
+    ActiveRecord::Base.logger.debug "sql: #{sql}"
 
-      sqlResult = ActiveRecord::Base.connection.select_all sql 
-      
-      ActiveRecord::Base.logger.debug "sqlResult: #{sqlResult}"
-      
-      sqlResult
-          
+    sqlResult = ActiveRecord::Base.connection.select_all sql 
+    
+    ActiveRecord::Base.logger.debug "sqlResult: #{sqlResult}"
+    
+    sqlResult
+        
+ end
+
+   def self.summary_dx_observed_vs_performed
    end
-   
-  end
+ 
+    #-------------------------------------------
+    # Diagnoses observed vs performed for each student
+    #-------------------------------------------    
+   def self.dx_observed_vs_performed
+   end
+     
+end
