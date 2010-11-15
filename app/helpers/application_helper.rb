8f5a536da4e596a0552b5c0f1e8652500a7c6303
lauriek334 <laurie@umich.edu>
2010-11-15 17:04:35 -0500

    Addded format_email helper to aplication_helper.rb Changed email formatting in the reports listed below by calling format_email `

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,7 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def format_email(email)
+    %Q[<a href="mailto:#{email}?subject=CLTP - ">#{email}</a>]
+   # "mailto:#{email}?subject=CLTP - ">#{email}
+  end
 end
